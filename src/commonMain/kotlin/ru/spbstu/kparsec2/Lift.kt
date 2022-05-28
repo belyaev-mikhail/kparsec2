11e121010fd6a0e55d5ea8d5795e5df819dbfcae
belyaev_mikhail <belyaev@kspt.icc.spbstu.ru>
2022-05-28 20:51:41 +0300

    fixes for zips

--- src/commonMain/kotlin/ru/spbstu/kparsec2/Lift.kt
+++ src/commonMain/kotlin/ru/spbstu/kparsec2/Lift.kt
@@ -1,5 +1,7 @@
 package ru.spbstu.kparsec2
 
+import ru.spbstu.kparsec2.parsers.combinators.namedParser
+
 @Suppress("UNCHECKED_CAST")
 private fun <T, T1> unliftInput(input: Input<T>): Input<T1> = when(input) {
     is ParsedInput<*, *> -> input.parsedSource.input as Input<T1>
@@ -10,7 +12,7 @@ fun <T0, T1, R> liftParser(
     parser: Parser<T1, R>,
     tokenParser: Parser<T0, T1>,
     ignore: Parser<T0, Unit>? = null
-): Parser<T0, R> = Parser {
+): Parser<T0, R> = namedParser({ "$parser" }) {
     val liftedInput = parsedInput(it, tokenParser, ignore)
     val result = parser(liftedInput)
     when (result) {
